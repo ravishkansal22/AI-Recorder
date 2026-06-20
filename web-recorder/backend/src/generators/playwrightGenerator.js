07db10043e1f219abadaaed3496e3e09cdec0455
Ravish Kansal <ravishkansalrk22@gmail.com>
2026-06-20 12:36:07 +0530

    files added

---
+++ web-recorder/backend/src/generators/playwrightGenerator.js
@@ -0,0 +1,45 @@
+function generate(events) {
+
+    let code = `
+import { test, expect } from '@playwright/test';
+
+test('Recorded Test', async ({ page }) => {
+`;
+
+    for (const event of events) {
+
+        if (event.type === "navigate") {
+
+            code += `
+    await page.goto('${event.url}');
+`;
+        }
+
+        if (event.type === "click") {
+
+            code += `
+    await page.click('${event.selector}');
+`;
+        }
+
+        if (event.type === "input") {
+
+            code += `
+    await page.fill(
+        '${event.selector}',
+        '${event.value}'
+    );
+`;
+        }
+    }
+
+    code += `
+});
+`;
+
+    return code;
+}
+
+module.exports = {
+    generate
+};
